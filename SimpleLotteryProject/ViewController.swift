2755d486c3e7b831a4e546ec5da58ca39198d71f
Daeyun <sweet_ray@naver.com>
2024-06-05 17:11:12 +0900

    Initial Commit

---
+++ SimpleLotteryProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleLotteryProject
+//
+//  Created by 권대윤 on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
